c39591543500c9c57aa81afd1503e2756d2177dc
Raghav Mangrola <dev@raghav.io>
2025-02-07 16:41:56 -0800

    Initial Commit

---
+++ WaterTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WaterTracker
+//
+//  Created by Raghav Mangrola on 2/7/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
